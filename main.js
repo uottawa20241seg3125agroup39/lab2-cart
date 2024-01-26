31bc1987d0e661d084d453815e2a73c5e01b3b6a
fackr jiang <44115200+skyfackr@users.noreply.github.com>
2024-01-26 15:52:53 -0500

    loading logic

--- main.js
+++ main.js
@@ -4,3 +4,39 @@ function loadpage(event, pageName) {
         document.getElementById("content").style.display = "block";
     }
 }
+function loadProducts()
+{
+    const element=$('#product-selection')
+    products.forEach(product => {
+        element.append(
+            `
+            <input type="checkbox" id="product-select-${product.id}" name="${product.name}" value="${product.name}">
+            <label for="product-select-${product.id}">${product.name}</label><br>
+            `
+        )
+    })
+    updateProductEmpty();
+    console.log("products loaded");
+}
+
+function updateProductEmpty()
+{
+    const element=$('#product-selection')
+    if (element.children.length>1)
+    {
+        $('#select-submission').hidden=false;
+        $('#empty-sign').hidden=true;
+        console.log("products not empty")
+    }
+    else {
+        $('#select-submission').hidden=true;
+        $('#empty-sign').hidden=false;
+        console.log("products empty")
+    }
+}
+window.onload = function() {
+    loadProducts();
+    $('#user').hidden=false;
+    $('#loading').hidden=true;
+    console.log("loaded");
+}
