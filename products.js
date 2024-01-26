dfb4e04df79a5012fe754dc9c620577a868b0dd2
fackr jiang <44115200+skyfackr@users.noreply.github.com>
2024-01-26 15:30:42 -0500

    Update products.js

--- products.js
+++ products.js
@@ -1,12 +1,12 @@
-var products = [
-    {"id":1,"name":"beef pizza","category":"normal"},
-    {"id":2,"name":"chicken pizza","category":"normal"},
-    {"id":3,"name":"veg pizza","category":"vegan"},
-    {"id":4,"name":"mushroom pizza","category":"vegan"},
-    {"id":5,"name":"pineapple pizza","category":"normal"},
-    {"id":6,"name":"pepperoni pizza","category":"normal"},
-    {"id":7,"name":"cheese pizza","category":"gluten-free"},
-    {"id":8,"name":"pork pizza","category":"gluten-free"},
-    {"id":9,"name":"sausage pizza","category":"normal"},
-    {"id":10,"name":"plain pizza","category":"normal"},
-]
+const products = [
+    {"id": 1, "name": "beef pizza", "category": "normal"},
+    {"id": 2, "name": "chicken pizza", "category": "normal"},
+    {"id": 3, "name": "veg pizza", "category": "vegan"},
+    {"id": 4, "name": "mushroom pizza", "category": "vegan"},
+    {"id": 5, "name": "pineapple pizza", "category": "normal"},
+    {"id": 6, "name": "pepperoni pizza", "category": "normal"},
+    {"id": 7, "name": "cheese pizza", "category": "gluten-free"},
+    {"id": 8, "name": "pork pizza", "category": "gluten-free"},
+    {"id": 9, "name": "sausage pizza", "category": "normal"},
+    {"id": 10, "name": "plain pizza", "category": "normal"},
+];
